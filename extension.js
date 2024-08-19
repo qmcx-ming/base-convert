36f60eeb385aa727a808c7d9743b9917882b1a42
lwm <2691517699@qq.com>
2024-08-19 22:18:45 +0800

    🐞 fix: 降低engine版本

--- extension.js
+++ extension.js
@@ -5,7 +5,7 @@ const { init } = require('./src');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('原神启动~');
+	console.log('进制转换插件启动~');
 	context.subscriptions.push(init());
 	const disposable = vscode.commands.registerCommand('baseConvert.helloWorld', function () {
 		vscode.window.showInformationMessage('Hello World from base-convert!');
